6e318697007929b6a2047c7b935a5683e2d84212
brad467388 <dream010436@gmail.com>
2024-02-22 21:00:16 +0800

    first commit

---
+++ temp.js
@@ -0,0 +1,64 @@
+// -----------------------------------------
+// // 使用let 優於 var
+// let x = 10;
+// // 常數宣告
+// const PI = 3.1415926;
+
+// // 計算圓面積
+// let radius = prompt('請輸入半徑：');
+// let area = radius * radius * PI;
+// // 字串 + 數字 (字串串接)
+// let result = "面積為：" + area;
+// console.log(radius, area, result);
+// document.write("<h2>" + result + "</h2>");
+
+
+// // alert(result);
+
+let email = "mary@gmail.com"
+console.log(email);
+console.log(email.length);
+console.log(email[email.length - 1]);
+
+for (let i = 0; i < email.length; i++) {
+    console.log(i, email[i]);
+}
+
+let z = null;
+console.log(z + 10);
+
+console.log(Math.PI);
+
+
+        // while(true){
+        //     let r = Math.floor(Math.random()*49)+1;
+        //     console.log(r);
+        //     if(r==50){
+        //         break;
+        //     };
+        // }
+
+
+        // -----------------------------------------
+
+        // let height = prompt('請輸入身高')
+        // let weight = prompt("請輸入體重")
+        // let bmi = weight / ((height / 100) ** 2)
+        
+        // let result = "<ul>";
+        // result += "<li>身高：" + height + "</li>";
+        // result += "<li>體重：" + weight + "</li>";
+        // result += "<li>BMI：" + bmi.toFixed(2) + "</li>";
+        // console.log("BMI:" + bmi);
+        // result += "</ul>";
+        // document.write(result);
+        // -----------------------------------------
+        // let email = "xxx@gmial.com";
+        // console.log(email);
+        // console.log(email.length);
+        // console.log(email[email.length - 1]);
+
+        // for (let i = 0; i < email.length; i++) {
+        //     console.log(i, email[i])
+        // }
+        
