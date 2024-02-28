9f2814a965b6ef57447827d7d7e12262c5a71c4e
brad467388 <dream010436@gmail.com>
2024-02-28 08:56:29 +0800

    使用DOM 方式取捯網頁元素賦值

---
+++ index.js
@@ -0,0 +1,101 @@
+
+//  產生亂數
+// for (let i = 0; i < 10; i++) {
+//     let r = Math.floor(Math.random() * 49) + 1;
+//     console.log(r)
+// }
+
+// while (true) {
+//     let r = getRandomInt(1, 49);
+//     console.log(r);
+//     if (r == 49) {
+//         break;
+//     }
+// }
+function getRandomInt(start, end) {
+    let x = Math.floor(Math.random() * end) + start;
+    return x;
+}
+
+document.write(Date() + '<hr>');
+
+let rows = [];
+for (let j = 0; j < 5; j++) {
+    let numbers = [];
+
+    while (true) {
+        let x = getRandomInt(1, 49);
+        if (!numbers.includes(x)) {
+            numbers.push(x);
+        }
+        if (numbers.length == 6) {
+            break;
+        }
+    }
+    numbers.sort(compare)
+    rows.push(numbers);
+}
+// 升續 
+function compare(a, b) {
+    return a - b;
+}
+
+console.log(rows)
+
+const lottoryEl = document.querySelector('#lottroy');
+
+
+for (let i = 0; i < rows.length; i++) {
+    result = rows[i].join(" , ");
+    // console.log(result);
+    lottoryEl.innerHTML += `<h3>第${i + 1}組號碼：${result}</h3><hr>`
+    // document.write(`<h3>第${i + 1}組號碼：${result}</h3><hr>`)
+}
+
+// let rows = [];
+// for (let j = 0; j < 5; j++) {
+//     let numbers = [];
+
+//     for (let i = 0; i < 6; i++) {
+//         let r = getRandomInt(1, 49);
+//         // document.write(`<h2>第${i + 1}組號碼為: ${r}</h2>`);
+//         numbers.push(r)
+//     }
+//     // console.log(numbers);
+//     rows.push(numbers);
+//     document.write(`<ol>第${j + 1}組 產生的號碼為 ${numbers}</ol>`)
+// }
+// console.log(rows);
+
+
+// x = "3.5";
+
+// document.write(parseInt(x));
+// document.write(Number(x));
+
+let user = {
+    name: "Jerry",
+    height: 177.3,
+    weight: 77.8,
+
+}
+
+console.log(user, typeof (user));
+console.log(user.name, user["name"])
+console.log(getBmi(user.height, user.weight))
+console.log(getBmi('180', '78'))
+//計算ＢＭＩ
+function getBmi(height, weight) {
+    let bmi = weight / (height / 100) ** 2;
+    if (isNaN(bmi)) {
+        return "數值錯誤!";
+    }
+    return bmi.toFixed(2);
+}
+
+
+const h1 = document.querySelector("h1");
+console.log(lottoryEl)
+console.log(h1)
+h1.innerHTML = "<u>大樂透</u>";
+h1.style.color = 'red';
